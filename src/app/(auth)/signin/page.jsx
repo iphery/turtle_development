b17234bff1ae2f7eed2dc443e14e796a121672c8
Putu Hery <iphery@gmail.com>
2024-10-02 21:45:01 +0800

    1

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -26,6 +26,7 @@ const SignIn = () => {
 
   const router = useRouter();
 
+  /*
   const fetch_data = async (uid) => {
     console.log("sini");
     console.log(uid);
@@ -41,6 +42,7 @@ const SignIn = () => {
     }
     setOnloadLogin(false);
   };
+  */
   const proceed_login = async () => {
     setOnloadLogin(true);
 
@@ -53,18 +55,9 @@ const SignIn = () => {
       //setUser(userCredential.user.uid);
       // fetch_data(userCredential.user.uid);
       console.log(userCredential.user.uid);
-      /*
-      cookie({ res }, "uid", userCredential.user.uid, {
-        maxAge: 7 * 24 * 60 * 60, // 7 days
-        path: "/", // Make it available across the whole site
-        httpOnly: true, // Prevents JavaScript from accessing the cookie
-        secure: true,
-        //secure: process.env.NODE_ENV === 'production', // Use Secure in production
-        sameSite: "Strict", // Prevents CSRF attacks
-      });
-*/
+
       nookies.set(null, "uid", userCredential.user.uid, {
-        maxAge: 60, //7 * 24 * 60 * 60, // 30 days
+        maxAge: 60,
         path: "/",
       });
       window.location.reload();
@@ -124,7 +117,7 @@ const SignIn = () => {
               />
             </div>
             <div className="mt-3 text-center text-white">
-              Don't have account ?
+              {"Don't have account ?"}
               <span
                 className="ml-2 cursor-default hover:text-primary"
                 onClick={() => {
@@ -138,61 +131,6 @@ const SignIn = () => {
         </div>
       </div>
     </div>
-    /*
-    <>
-      <div className="flex min-h-screen items-center justify-center bg-boxdark-2 p-10">
-        <div className="flex w-full flex-col justify-center border">
-          <div>aa</div>
-          <div className="flex w-full rounded-sm border border-strokedark bg-boxdark shadow-default sm:w-1/3">
-            <div className="w-full p-4 sm:p-12.5 ">
-              <h2 className="mb-9 text-2xl font-bold text-black  text-white sm:text-title-xl2">
-                Login
-              </h2>
-
-              <CommonInputIcon
-                placeholder={"Enter your email"}
-                type={"text"}
-                error={inputEmailError}
-                errorMessage={inputEmailMessage}
-                onInputChange={(val) => {
-                  setInputEmail(val);
-                }}
-                onKeyChange={() => {
-                  setEmailError(false);
-                }}
-              >
-                <HiOutlineMail className="h-7 w-7"></HiOutlineMail>
-              </CommonInputIcon>
-              <div className="mb-5" />
-              <CommonInputIcon
-                placeholder={"Enter your password"}
-                type={"password"}
-                error={inputPasswordError}
-                errorMessage={inputPasswordMessage}
-                onInputChange={(val) => {
-                  setInputPassword(val);
-                }}
-                onKeyChange={() => {
-                  setPasswordError(false);
-                }}
-              >
-                <HiLockClosed className="h-7 w-7" />
-              </CommonInputIcon>
-              <div className="mb-10" />
-              <div className="w-full">
-                <CommonButtonFull
-                  label={"Login"}
-                  onClick={proceed_login}
-                  onload={onloadLogin}
-                  disabled={disabledLogin}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-    */
   );
 };
 
