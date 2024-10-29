6558ca82cfc2f80df591fce7a654931cad7c00b2
Putu Hery <iphery@gmail.com>
2024-10-29 15:47:15 +0800

    1

--- src/app/stockopname/[id_report]/page.jsx
+++ src/app/stockopname/[id_report]/page.jsx
@@ -80,8 +80,9 @@ export default function Page({ params }) {
         }
       } else {
         console.log("belum join");
+
         setStatusJoin(false);
-        setStatusInvite(false);
+        setStatusInvite(true);
         setIsLead(false);
       }
 
