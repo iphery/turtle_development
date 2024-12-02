f9ea231826f23b43c7e6943b2de0e30240c307ea
Putu Hery <iphery@gmail.com>
2024-12-02 20:30:35 +0800

    a

--- src/app/stockout/page.jsx
+++ src/app/stockout/page.jsx
@@ -410,7 +410,7 @@ export default function PartsOut() {
                         </div>
                       </div>
                     </PageCard>
-                    <div className="mb-5">fff2</div>
+                    <div className="mb-5">fff4</div>
                     <PageCard>
                       <div className="flex-row sm:flex">
                         <div className="mb-2 flex flex-row items-center sm:mb-0 sm:w-full">
@@ -427,42 +427,44 @@ export default function PartsOut() {
                               }}
                               onKeyChange={(event) => {
                                 //alert(event.key);
-                                const match = keyword.match(
-                                  /^(.*?)20FF\/FF\/FF FF:FF:FF$/,
-                                );
-                                if (match) {
-                                  const result = match[1];
-                                  alert(result);
-                                }
-                                /*
+                                console.log(event.key);
                                 if (event.key === "Enter") {
-                                  const filterProduct = products.filter(
-                                    (item) => {
-                                      const result =
-                                        item.id_product === keyword;
-
-                                      return result;
-                                    },
+                                  const match = keyword.match(
+                                    /^(.*?)20FF\/FF\/FF FF:FF:FF$/,
                                   );
+                                  console.log(match);
+                                  if (match) {
+                                    const scanResult = match[1].trim();
+                                    console.log(match[1]);
 
-                                  //  console.log(filterProduct);
+                                    const filterProduct = products.filter(
+                                      (item) => {
+                                        const result =
+                                          item.barcode === scanResult;
 
-                                  if (filterProduct.length > 0) {
-                                    SetScanProcessing(true);
-                                    const result = filterProduct[0];
-                                    // console.log(result.id_product);
-                                    setTempIdPart(result.id_product);
-                                    setTempItem(result.description);
-                                    setTempUnit(result.unit);
-                                    setTempAvailableQuantity(
-                                      result.available_quantity,
+                                        return result;
+                                      },
                                     );
-                                    setTempQuantity(1);
+                                    console.log(filterProduct);
 
-                                    //   console.log(tempIdPart);
+                                    //  console.log(filterProduct);
+
+                                    if (filterProduct.length > 0) {
+                                      SetScanProcessing(true);
+                                      const result = filterProduct[0];
+                                      // console.log(result.id_product);
+                                      setTempIdPart(result.id_product);
+                                      setTempItem(result.description);
+                                      setTempUnit(result.unit);
+                                      setTempAvailableQuantity(
+                                        result.available_quantity,
+                                      );
+                                      setTempQuantity(1);
+
+                                      //   console.log(tempIdPart);
+                                    }
                                   }
                                 }
-                                  */
                               }}
                               placeholder={"Search"}
                             >
