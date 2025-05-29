20563d05b17e6932afcc3b62e55edcc1f8cc911d
Putu Hery <iphery@gmail.com>
2025-05-29 21:39:38 +0800

    change server endpoint

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,7 @@
-export const API_URL = "https://logbook.pbfobat.com/api";
-export const IMAGE_URL = "https://logbook.pbfobat.com/public/images";
-export const ASSET_URL = "https://logbook.pbfobat.com/public/assets";
+// export const API_URL = "https://logbook.pbfobat.com/api";
+// export const IMAGE_URL = "https://logbook.pbfobat.com/public/images";
+// export const ASSET_URL = "https://logbook.pbfobat.com/public/assets";
+
+export const API_URL = "https://mipa.farmaguru.cloud/api";
+export const IMAGE_URL = "https://mipa.farmaguru.cloud/public/images";
+export const ASSET_URL = "https://mipa.farmaguru.cloud/public/assets";
